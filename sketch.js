ffc43391a36e8e6c64d6b4985e4820b7e3c1e938
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2021-11-16 12:25:58 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
-var weight = [35,38,42,45,43,34,36,41,48,32];
+var pesos = [35,38,40,45];
+
+function media_pesos(){
+    var soma = pesos[0] + pesos[1] + pesos[2] + pesos[3];
+    var media = soma / media.length;
+    console.log(media)
+}
+
 
 function setup() {
   createCanvas(400,400);
-
+  media_pesos();
 }
 
 function draw() 
