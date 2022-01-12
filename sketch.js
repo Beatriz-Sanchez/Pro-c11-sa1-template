af23b1866684d3f710ee6ea89043141a8a7ee888
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2022-01-12 09:56:15 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,11 +3,11 @@ var pesos = [35,38,40,45];
 function media_pesos(){
     var soma = 0;
     
-    //preencha o loop FOR com o valor de início e o de limite para percorrer a matriz
+    //preencha o loop FOR com o valor de início e o de limite para percorrer a matriz de pesos
     //o valor de início deve ser o número do primeiro índice da matriz
     //o limite deve ser o número de itens que a matriz tem
     for (var i = ; i < ; i++){
-        //adicione o valor de cada item da matriz à variável soma
+        //adicione o valor de cada peso à variável soma
        soma +=    
     }
     
